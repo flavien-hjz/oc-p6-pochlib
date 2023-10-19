82b4ca7a8bba542ea46987a8f4660464668268b7
Flavien Hijazi <flavien.hijazi@gmail.com>
2023-10-19 17:56:21 +0200

    Implement research via enter and error message

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+let bookList = JSON.parse(localStorage.getItem('bookList'));
+
+if (!bookList) {
+  bookList = [{
+    bookTitle: 'Un titre',
+    bookId: '1',
+    bookAuthor: 'Un auteur',
+    bookDescription: 'Une description',
+    bookImage: 'images/unavailable.png'
+  }, {
+    bookTitle: 'Un 2e titre',
+    bookId: '2',
+    bookAuthor: 'Un 2e auteur',
+    bookDescription: 'Une 2e description',
+    bookImage: 'Une 2e image'
+  }];
+};
+
+function saveToStorage() {
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+
+console.log(bookList);
+
+function displayBookList() {
+  let myBookList = document.getElementById('content');
+  myBookList = `
+  <div class="result-box js-result-box">
+    <div>
+      <i class="fa-regular fa-bookmark js-bookmark"></i>
+    </div>
+    <p class="box-title">Titre : ${bookList[0].bookTitle}</p>
+    <p class="box-id">Id : ${bookList[0].bookId}</p>
+    <p class="box-author">Auteur : ${bookList[0].bookAuthor}</p>
+    <p class="box-description">Description : ${bookList[0].bookDescription}</p>
+    <div class="cover">
+      <img src="${bookList[0].bookImage}">
+    </div>
+  </div>`
+
+  document.getElementById('content').insertAdjacentHTML('beforeend', myBookList);
+};
+
+displayBookList();
+
 function addBook() {
   document.querySelector('.js-add-button').addEventListener('click', () => {
     displaySearchSection();
@@ -8,16 +53,19 @@ addBook();
 
 function displaySearchSection() {
   document.querySelector('.js-search-container').innerHTML = `
-    <p class="input-label">Titre du livre</p>
-    <input type="search" class="js-title-input" placeholder="Renseignez le titre du livre" >
-    <p class="input-label">Auteur</p>
-    <input type="search" class="js-author-input" placeholder="Renseignez l'auteur" >
+    <form>
+      <label for="title" class="input-label">Titre du livre</label>
+      <input type="text" id="title" name="title" class="js-title-input" placeholder="Renseignez le titre du livre">
+      <label for="author" class="input-label">Auteur</label>
+      <input type="text" id="author" name="author" class="js-author-input" placeholder="Renseignez l'auteur">
+    </form>
     <div class="buttons-box">
       <button class="button green-button js-search-button">Rechercher</button>
       <button class="button red-button js-cancel-button">Annuler</button>
-    </div>`;
+    </div>
+    <span id="error-message"></span>`;
   cancelSearch();
-  searchAPI();
+  launchSearch();
 };
 
 function cancelSearch() {
@@ -35,8 +83,7 @@ function hideSearchSection() {
     </div>`;
 }
 
-function searchAPI() {
-  document.querySelector('.js-search-button').addEventListener('click', async () => {
+async function searchAPI() {
     document.querySelector('.js-result-section').innerHTML = `
     <h2 class="sub-title">Résultats de la recherche</h2>
 		<div class="result-box-grid js-result-box-grid">
@@ -46,11 +93,10 @@ function searchAPI() {
     const title = document.querySelector('.js-title-input').value;
     const author = document.querySelector('.js-author-input').value;
     const url = 'https://www.googleapis.com/books/v1/volumes?q='+title+author;
-    console.log(url);
 
     const response = await fetch(url);
     const books = await response.json();
-    console.log(books);
+    //console.log(books);
 
     books.items.forEach((book) => {
 
@@ -66,12 +112,20 @@ function searchAPI() {
         bookImage = book.volumeInfo.imageLinks.smallThumbnail
       };
 
+      let bookTitle = truncateText(book.volumeInfo.title, 100);
+      let bookId = book.id;
+      let bookAuthor = book.volumeInfo.authors[0];
+
       const newResult = `
         <div class="result-box js-result-box">
-          <i class="fa-regular fa-bookmark"></i>
-          <p class="box-title">Titre : ${book.volumeInfo.title}</p>
-          <p class="box-id">Id : ${book.id}</p>
-          <p class="box-author">Auteur : ${book.volumeInfo.authors[0]}</p>
+          <div class="js-bookmark"
+            data-book-id="${bookId}"
+            data-book-author="${bookAuthor}">
+            <i class="fa-regular fa-bookmark js-bookmark"></i>
+          </div>
+          <p class="box-title">Titre : ${bookTitle}</p>
+          <p class="box-id">Id : ${bookId}</p>
+          <p class="box-author">Auteur : ${bookAuthor}</p>
           <p class="box-description">Description : ${bookDescription}</p>
           <div class="cover">
             <img src="${bookImage}">
@@ -81,7 +135,49 @@ function searchAPI() {
       document.querySelector('.js-result-box-grid').insertAdjacentHTML('beforeend', newResult);
 
     });
-  })
+
+    document.querySelectorAll('.js-bookmark').forEach(item => {
+      item.addEventListener('click', event => {
+        console.log(item.dataset.bookId);
+        console.log(item.dataset.bookAuthor);
+      })
+    })
+    
+};
+
+function launchSearch() {
+
+  document.querySelector('.js-search-button').addEventListener('click', () => {
+
+    const titleInput = document.querySelector('.js-title-input').value;
+    const authorInput = document.querySelector('.js-author-input').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (!titleInput || !authorInput) {
+      errorMessage.textContent = "Merci de renseigner un titre et un auteur";
+    } else {
+      errorMessage.textContent = "";
+      searchAPI();
+    };
+
+  });
+
+  document.body.addEventListener('keyup', (event) => {
+
+    const titleInput = document.querySelector('.js-title-input').value;
+    const authorInput = document.querySelector('.js-author-input').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (event.key === 'Enter') {
+
+      if (!titleInput || !authorInput) {
+        errorMessage.textContent = "Merci de renseigner un titre et un auteur";
+      } else {
+        errorMessage.textContent = "";
+        searchAPI();
+      };
+    };
+  });
 };
 
 function truncateText(string, n){
